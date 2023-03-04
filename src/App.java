bde96d96d31634df739c93c4c93c6d1a6ea7dd9a
hasbiesyawaluddin <hasbiesyawaluddin@gmail.com>
2023-03-04 19:02:22 +0700

    Hasbie Syawaluddin

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hasbie");
+        System.out.println("Syawaluddin");
 
         int[] array = new int[10];
         Arrays.sort(array);
